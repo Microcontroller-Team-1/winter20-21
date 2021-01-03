c2b0112d9a7151acae8177d71fb53d286b518bdc
Amjad-Malik <muhammad-amjad-bin.abdul-malik@stud.hshl.de>
2021-01-03 23:39:54 +0100

    Create Water.h

---
+++ Project_Week_9/Smart_Fire_Alarm/Water.h
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+#ifndef WATER
+#define WATER
+
+class Smart_Home_System;
+class Water
+{
+public:
+
+    void turn_on_water_sprinkler();
+
+};
+
+#endif
