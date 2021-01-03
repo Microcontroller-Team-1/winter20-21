e70482acafc32e107bf2db25de413ffa6cef78aa
Amjad-Malik <muhammad-amjad-bin.abdul-malik@stud.hshl.de>
2021-01-03 23:39:48 +0100

    Create Sound_system.h

---
+++ Project_Week_9/Smart_Fire_Alarm/Sound_system.h
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+#ifndef SOUND SYSTEM
+#define SOUND SYSTEM
+
+class Smart_Home_System;
+class SoundSystem
+{
+public:
+
+    void play_sound();
+    void notone();
+
+
+
+
+
+
+};
+
+#endif
