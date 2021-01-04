b0ac818aae00b081b8e3c966db4766815a996795
Amirul Hakimi <muhammad-amirul-hakimi.bin-zaprunnizam@stud.hshl.de>
2021-01-04 11:35:24 +0100

    Create Door.h

---
+++ Project_Week_9/Smart Door Lock/Door.h
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+#ifndef DOOR
+#define DOOR
+
+class Smart_Door_Lock;
+class Door
+{
+public:
+
+    void door_lock();
+    void door_unlock();
+    void closeDoor();
+    void openDoor();
+
+
+};
+
+#endif
