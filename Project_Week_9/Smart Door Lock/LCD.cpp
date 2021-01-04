581a0888e3d5848fdc5e52f94cc85628a43b0ba2
Amirul Hakimi <muhammad-amirul-hakimi.bin-zaprunnizam@stud.hshl.de>
2021-01-04 11:35:28 +0100

    Create LCD.cpp

---
+++ Project_Week_9/Smart Door Lock/LCD.cpp
@@ -0,0 +1,14 @@
+#include "LCD.h"
+#include <iostream>
+
+void lcd::lcd_display("enter passcode"); {
+
+}
+
+void lcd::lcd_display("correct passcode"); {
+
+}
+
+void lcd::lcd_display("incorrect passcode"); {
+
+}
