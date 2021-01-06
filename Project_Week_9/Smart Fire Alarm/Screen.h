6d97f4e42408d9ef829f13986bc3d1f59e64c2fc
Amjad-Malik <muhammad-amjad-bin.abdul-malik@stud.hshl.de>
2021-01-06 15:17:19 +0100

    Create Screen.h

---
+++ Project_Week_9/Smart Fire Alarm/Screen.h
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+#ifndef SCREEN
+#define SCREEN
+
+class Smart_Home_System;
+class Screen
+{
+public:
+
+    void LCD_Display_All_Clear();
+    void LCD_Display_Evacuate();
+
+
+
+
+
+
+};
+
+#endif
