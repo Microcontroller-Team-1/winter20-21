4d9f7ab7c507c650b675cfec500d27169d8ab8dd
faridshabri <muhammad-farid-izwan.bin-mohamad-shabri@stud.hshl.de>
2021-01-11 00:50:50 +0100

    Create SOUNDSYSTEM.cpp

---
+++ Project_Week_9/Smart Light/Test/SOUNDSYSTEM.cpp
@@ -0,0 +1,14 @@
+#include "SOUNDSYSTEM.h"
+#include <iostream>
+
+void SoundSystem :: music_on()
+{  
+	cout << "Music is play" << endl;
+
+}
+void SoundSystem :: music_off()
+
+{
+	cout << "Music is not play" << endl;
+
+}
