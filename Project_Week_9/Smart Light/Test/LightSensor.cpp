711fc354d958a8438787eac60cab581b62d5d28d
faridshabri <muhammad-farid-izwan.bin-mohamad-shabri@stud.hshl.de>
2021-01-11 00:50:20 +0100

    Create LightSensor.cpp

---
+++ Project_Week_9/Smart Light/Test/LightSensor.cpp
@@ -0,0 +1,14 @@
+#include "LightSensor.h"
+#include <iostream>
+
+void LightSensor::light_detected() 
+{
+	cout << "Light is detected at outside" << endl;
+}
+void LightSensor::nolight_detected() 
+{
+	cout << "No light is detected at outside" << endl;
+
+}
+
+
