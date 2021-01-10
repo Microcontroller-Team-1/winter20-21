f41ba33997f847cfbb0332e81837317890b2fa03
faridshabri <muhammad-farid-izwan.bin-mohamad-shabri@stud.hshl.de>
2021-01-11 00:47:10 +0100

    Create LightSensor.h

---
+++ Project_Week_9/Smart Light/Code/LightSensor.h
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+#ifndef  LIGHTSENSOR 
+#define LIGHTSENSOR
+
+
+
+class SmartHomeSystem;
+class LightSensor
+{
+public:
+
+	string material;
+
+	void light_detected();
+	void nolight_detected();
+
+
+
+
+};
+#endif
