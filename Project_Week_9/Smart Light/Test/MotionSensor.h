e75b23492750c1ed082277b01989732a2501ec01
faridshabri <muhammad-farid-izwan.bin-mohamad-shabri@stud.hshl.de>
2021-01-11 00:50:37 +0100

    Create MotionSensor.h

---
+++ Project_Week_9/Smart Light/Test/MotionSensor.h
@@ -0,0 +1,20 @@
+
+#include <iostream>
+using namespace std;
+
+#ifndef MOTION
+#define MOTION
+
+class SmartHomeSystem;
+class Motion_Sensor
+{
+public:
+
+	string material;
+
+
+	bool motion_detected();
+
+
+};
+#endif
