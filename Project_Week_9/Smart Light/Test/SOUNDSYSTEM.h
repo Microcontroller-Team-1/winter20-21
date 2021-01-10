4ccb2228f4a7c2a25a3c5ee11c872c50cfbe8767
faridshabri <muhammad-farid-izwan.bin-mohamad-shabri@stud.hshl.de>
2021-01-11 00:50:54 +0100

    Create SOUNDSYSTEM.h

---
+++ Project_Week_9/Smart Light/Test/SOUNDSYSTEM.h
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+#ifndef  SOUNDSYSTEM
+#define SOUNDSYSTEM
+
+class SmartHomeSystem;
+class SoundSystem
+{
+public:
+
+	string material;
+
+	void music_on();
+	void music_off();
+
+
+
+
+};
+#endif
+
