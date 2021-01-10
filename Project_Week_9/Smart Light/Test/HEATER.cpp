9448de8da27e5c256ebd886d88d37366f040cb25
faridshabri <muhammad-farid-izwan.bin-mohamad-shabri@stud.hshl.de>
2021-01-11 00:48:27 +0100

    Create HEATER.cpp

---
+++ Project_Week_9/Smart Light/Test/HEATER.cpp
@@ -0,0 +1,11 @@
+#include "HEATER.h"
+#include <iostream>
+
+void Heater :: heater_on() 
+{
+	cout << "Heater is on" << endl;
+}
+void Heater :: heater_off()
+{
+	cout << "Heater is off" << endl;
+}
