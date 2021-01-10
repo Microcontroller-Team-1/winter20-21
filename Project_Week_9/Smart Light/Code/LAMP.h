0bd93660118e2db3879e3c641ce7c10e51c402e7
faridshabri <muhammad-farid-izwan.bin-mohamad-shabri@stud.hshl.de>
2021-01-11 00:47:02 +0100

    Create LAMP.h

---
+++ Project_Week_9/Smart Light/Code/LAMP.h
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+#ifndef LIGHT
+#define LIGHT
+
+class SmartHomeSystem;
+class  Lamp
+{
+
+public:
+
+	string colour;
+	double brightness;
+
+
+	void On_light();
+	void Off_light();
+
+
+
+
+
+};
+#endif
+
