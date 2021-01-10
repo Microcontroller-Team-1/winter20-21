2e3a68a556bc39a5bec682bb53dabd649868424a
faridshabri <muhammad-farid-izwan.bin-mohamad-shabri@stud.hshl.de>
2021-01-11 00:46:54 +0100

    Create HEATER.h

---
+++ Project_Week_9/Smart Light/Code/HEATER.h
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+
+#ifndef  HEATER
+#define HEATER
+
+class SmartHomeSystem;
+class Heater
+{
+public:
+
+	string material;
+
+	void heater_on();
+	void heater_off();
+
+
+
+
+};
+#endif
