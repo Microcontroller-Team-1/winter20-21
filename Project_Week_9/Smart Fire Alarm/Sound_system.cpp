728b3f2c728818f380621dd46358d842a053f2ca
Amjad-Malik <muhammad-amjad-bin.abdul-malik@stud.hshl.de>
2021-01-11 00:23:36 +0100

    Update Sound_system.cpp

--- Project_Week_9/Smart Fire Alarm/Sound_system.cpp
+++ Project_Week_9/Smart Fire Alarm/Sound_system.cpp
@@ -2,9 +2,10 @@
 #include <iostream>
 
 void SoundSystem:: play_sound() {
+	cout << "Emergency Sound is played (NOTIFY USER)" << endl;
 
 }
 
 void SoundSystem::notone() {
-
+	cout << "No Sound is played" << endl;
 }
