227f5f8f6730e4c25f7348a97e3d258a32524ddc
Amjad-Malik <muhammad-amjad-bin.abdul-malik@stud.hshl.de>
2021-01-11 00:23:10 +0100

    Update Door.cpp

--- Project_Week_9/Smart Fire Alarm/Door.cpp
+++ Project_Week_9/Smart Fire Alarm/Door.cpp
@@ -2,13 +2,16 @@
 #include <iostream>
 
 void Door::door_lock() {
+	cout << "Door is locked" << endl;
 
 }
 
 void Door::door_unlock() {
+	cout << "Door is unlocked" << endl;
 
 }
 
 void Door::open_door() {
+	cout << "Door is opened" << endl;
 
 }
