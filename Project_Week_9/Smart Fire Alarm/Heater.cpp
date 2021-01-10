2b5ab0538f062c62260aa5ed63b12df02ac3b633
Amjad-Malik <muhammad-amjad-bin.abdul-malik@stud.hshl.de>
2021-01-11 00:23:16 +0100

    Update Heater.cpp

--- Project_Week_9/Smart Fire Alarm/Heater.cpp
+++ Project_Week_9/Smart Fire Alarm/Heater.cpp
@@ -2,13 +2,13 @@
 #include <iostream>
 
 void Heater :: turn_on_heater(){
-    
+    cout << "Heater is on" << endl;
 }
 
 void Heater :: turn_off_heater(){
-    
+    cout << "Heater is off" << endl;
 }
 
 void Heater :: gettemperature(){
-    
+    cout << "Is there any smoke?" << endl;
 }
