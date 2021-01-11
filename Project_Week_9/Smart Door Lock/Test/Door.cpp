b19938f8d3a4ff57fba1b8897531c8eb4d2ba585
Amirul Hakimi <muhammad-amirul-hakimi.bin-zaprunnizam@stud.hshl.de>
2021-01-11 11:27:36 +0100

    Create Door.cpp

---
+++ Project_Week_9/Smart Door Lock/Test/Door.cpp
@@ -0,0 +1,18 @@
+#include "Door.h"
+#include <iostream>
+
+void Door::door_lock() {
+	cout << "Door Locked" << endl;
+}
+
+void Door::door_unlock() {
+	cout << "Door Unlocked" << endl;
+}
+
+void Door::openDoor() {
+	cout << "Door Opened" << endl;
+}
+
+void Door::closeDoor() {
+	cout << "Door Closed" << endl;
+}
