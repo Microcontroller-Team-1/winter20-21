44c96e2219d64d80a83b3e2788be8d8dddfe91db
Amirul Hakimi <muhammad-amirul-hakimi.bin-zaprunnizam@stud.hshl.de>
2021-01-11 11:27:46 +0100

    Create LCD.h

---
+++ Project_Week_9/Smart Door Lock/Test/LCD.h
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+#ifndef LCD
+#define LCD
+
+class Smart_Door_Lock;
+class lcd
+{
+public:
+
+    void lcd_display();
+    void lcd_wrong();
+};
+
+#endif
